64264c8fedf2fc0f314f8c9394ab53d1e6741dab
notshanxx <manuelmarkvince@gmail.com>
2024-11-30 15:19:00 +0800

    fis not showing book when done adding

--- scripts/google-sign-in/syncToGoogleBook.js
+++ scripts/google-sign-in/syncToGoogleBook.js
@@ -3,48 +3,51 @@ import Announce from "../component/Announce.js";
 // Local Storage to Google Books
 const syncToGoogleBtn = document.getElementById("sync-book-btn");
 
-
 syncToGoogleBtn.addEventListener("click", () => {
   const book = JSON.parse(localStorage.getItem("favorite")) || [];
 
   // prompt user to confirm if they want to sync
   if (!book.length) {
     alert("No books to sync. Add Books First");
-    return
-  }
-  if (!confirm("Your Saved Books will be Synced to Google Books. Are you sure you want to continue?")) {
-    
     return;
   }
-  if (!confirm(book.length + " Books to be added: \n"+(book.map(item => item.volumeInfo.title).join("\n"))) ) {
+  if (
+    !confirm(
+      "Your Saved Books will be Synced to Google Books. Are you sure you want to continue?"
+    )
+  ) {
     return;
   }
-
+  if (
+    !confirm(
+      book.length +
+        " Books to be added: \n" +
+        book.map((item) => item.volumeInfo.title).join("\n")
+    )
+  ) {
+    return;
+  }
 
   // request again for token
-    
-    const client = google.accounts.oauth2.initTokenClient({
-      client_id: '695259268184-fjn31kr2gpj39jks7udk93s1lonvclk6.apps.googleusercontent.com',
-      scope: 'https://www.googleapis.com/auth/books',
-      callback: syncToGoogleBook,
-    });
-    client.requestAccessToken();
-  
+
+  const client = google.accounts.oauth2.initTokenClient({
+    client_id:
+      "695259268184-fjn31kr2gpj39jks7udk93s1lonvclk6.apps.googleusercontent.com",
+    scope: "https://www.googleapis.com/auth/books",
+    callback: syncToGoogleBook,
+  });
+  client.requestAccessToken();
 
   // let book = JSON.parse(localStorage.getItem("favorite")) || [];
   // if (book.length === 0) {
   //   return;
   // }
   // syncToGoogleBook();
-  
-
-
 });
 
 // sync to google books
 
 function syncToGoogleBook(response) {
-  
   const book = JSON.parse(localStorage.getItem("favorite")) || [];
   if (book.length === 0) {
     return;
@@ -52,29 +55,29 @@ function syncToGoogleBook(response) {
 
   // looping all the books and add them to google books using the book id
 
-  book.forEach(data => {
-    fetch('https://www.googleapis.com/books/v1/mylibrary/bookshelves/0/addVolume', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${response.access_token}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        'volumeId': data.id
+  book.forEach((data) => {
+    fetch(
+      "https://www.googleapis.com/books/v1/mylibrary/bookshelves/0/addVolume",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${response.access_token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          volumeId: data.id,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // show google books
+        //refresh google books
+        handleResponse(response);
       })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      Announce(`Synced ${data.volumeId} to Google Books`)
-    })
-    .catch(error => {
-      console.error('Error fetching user info:', error);
-      Announce('Error syncing books to Google Books');
-    });
+      .catch((error) => {
+        console.error("Error fetching user info:", error);
+      });
   });
-  
-  // show google books
-  //refresh google books
-  handleResponse(response)
 }
